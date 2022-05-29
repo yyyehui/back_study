cc87be7733d396952c6e5f36faa9b69bf5de816e
sseyeon <seyeon9809@gmail.com>
2022-05-29 16:41:24 +0900

    chapter 7. 컴포넌트 스캔

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,13 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
+
+    @Autowired // ac.getBean(MemberRepository.class) 처럼 동작
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;   // memberRepository 의 생성자를 통해 구현체를 정의함 [생성자 주입]
                                                     // 기획자를 통해 배역을 정함, 오로지 추상화에만 의존함, DIP를 준수함
